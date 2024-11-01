e17618deb807201941a7ffde6f2eb3ddae9a6041
SabirovaOlya <sabirovaolya44@gmail.com>
2024-11-01 18:04:55 +0500

    auth handler

--- auth/auth_handler.py
+++ auth/auth_handler.py
@@ -8,8 +8,8 @@ from jose import jwt
 load_dotenv()
 
 # Constants
-ACCESS_TOKEN_EXPIRE_MINUTES = 30  # 30 minutes
-REFRESH_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7 # 7 days
+ACCESS_TOKEN_EXPIRE_MINUTES = 30  # minutes
+REFRESH_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7  # days
 ALGORITHM = "HS256"
 JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
 JWT_REFRESH_SECRET_KEY = os.getenv('JWT_REFRESH_SECRET_KEY')
